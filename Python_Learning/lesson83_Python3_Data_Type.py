18514dded903716df0a859d2dcbbb151c4adb326
xiangxing98 <xiangxing985529@163.com>
2020-05-30 23:00:05 +0800

    update vai

--- Python_Learning/lesson83_Python3_Data_Type.py
+++ Python_Learning/lesson83_Python3_Data_Type.py
@@ -14,8 +14,8 @@
 # here put the import lib
 
 
-counter = 100          # 整型变量
-miles = 1000.0       # 浮点型变量
+counter = 100      # 整型变量
+miles = 1000.0     # 浮点型变量
 name = "Stone"     # 字符串
 
 print(counter)
