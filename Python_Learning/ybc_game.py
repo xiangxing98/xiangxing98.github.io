02f1c66aa9d3b278fa5922d188651e4a0550eb7f
xiangxing98 <xiangxing985529@163.com>
2023-12-10 20:39:45 +0800

    update ybc_game, add on_mouse_down and on_mouse_up

--- Python_Learning/ybc_game.py
+++ Python_Learning/ybc_game.py
@@ -39,3 +39,50 @@ def on_mouse_down():
     dress.image = random.choice(dress_list)
     
 ybc_game.go()
+
+
+# -*- encoding: utf-8 -*-
+# !/usr/bin/env python
+'''
+@File    :   ybc_game.py
+@Time    :   2023/12/10 20:25:03
+@Author  :   Stone_Hou
+@Version :   1.0
+@Contact :   xiangxing985529@163.com
+@License :   (C)Copyright 2010-2021, Stone_Hou
+@Desc    :   None
+@Refer   :   https://github.com/xiangxing98
+'''
+
+# here put the import lib
+
+
+
+'''
+import ybc_game
+import random
+
+ybc_game.title('百变背景')
+ybc_game.size(600, 800)
+
+bg = ybc_game.actor('bg1')
+zy = ybc_game.actor('zy1', [300, 420])
+
+bg_list = ['bg1', 'bg2', 'bg3', 'bg4', 'bg5']
+zy_list = ['zy1', 'zy2', 'zy3', 'zy4', 'zy5']
+
+def update():
+    bg.draw()
+    zy.draw()
+
+# 按下鼠标，随机切换背景图片
+def on_mouse_down():
+    bg.image = random.choice(bg_list)
+
+# 松开鼠标，随机切换壮猿图片
+def on_mouse_up():
+    zy.image = random.choice(zy_list)
+    
+ybc_game.go()
+
+'''
